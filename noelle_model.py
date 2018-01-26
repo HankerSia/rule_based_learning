eac19a74d8ed1633d57b9c78113368a6b0ccd719
jstansil <jdstansil@gmail.com>
2018-01-25 17:26:58 -0800

    initial training successful, and import/export functions for weights are implemented

--- noelle_model.py
+++ noelle_model.py
@@ -2,14 +2,16 @@ import numpy as np
 
 class model:
     def __init__(self):
-        self.learning_rate = 0.05
         
         #likelihood of 0's in rule categories
-        self.pos_rule_dist = 0.2
+        self.pos_rule_dist = 0.6
         self.neg_rule_dist = 0.8
 
         #bias value used to generate sparse representations
         self.sparsity_bias = -3.0
+
+        #learning rate
+        self.learning_rate = 0.05
 
         #inital randomized synapses, exclude layers with static weights
         self.degraded_to_hidden = (1*np.random.random((9,25))-0.5) #bounded by [-0.5, 0.5)
@@ -17,11 +19,10 @@ class model:
         self.inst_to_category = (1*np.random.random((17, 2))-0.5) #bounded by [-0.5, 0,5)
         self.working_to_hidden = (2*np.random.random((25,25))-1) #no bounds, [-1.0, 1.0)
         self.hidden_to_category = (1*np.random.random((25,2))-0.5) #bounded by [-0.5, 0.5)
-
         
         #lists containing activations for each layer
-        self.stim_size = 0
-        self.stim_angle = 0
+        self.stim_size = []
+        self.stim_angle = []
         self.rules = []
         self.degraded_stim = []
         self.working = []
@@ -40,6 +41,51 @@ class model:
     def softmax(self, inputs):
         return np.exp(inputs) / float(sum(np.exp(inputs)))
 
+    #imports weight matricies from text files
+    def importWeights(self):
+        self.degraded_to_hidden = np.loadtxt('degraded_to_hidden.txt')
+        self.rules_to_working = np.loadtxt('rules_to_working.txt')
+        self.inst_to_category = np.loadtxt('inst_to_category.txt')
+        self.working_to_hidden = np.loadtxt('working_to_hidden.txt')
+        self.hidden_to_category = np.loadtxt('hidden_to_category.txt')
+
+    #exports each weight matrix to 5 files
+    def exportWeights(self): 
+        f = open('degraded_to_hidden.txt', 'w')
+        for syn in self.degraded_to_hidden:
+            for val in syn:
+                f.write("%s " % val)
+            f.write("\n")
+        f.close()
+
+        f = open('rules_to_working.txt', 'w')
+        for syn in self.rules_to_working:
+            for val in syn:
+                f.write("%s " % val)
+            f.write("\n")
+        f.close()
+
+        f = open('inst_to_category.txt', 'w')
+        for syn in self.inst_to_category:
+            for val in syn:
+                f.write("%s " % val)
+            f.write("\n")
+        f.close()
+
+        f = open('working_to_hidden.txt', 'w')
+        for syn in self.working_to_hidden:
+            for val in syn:
+                f.write("%s " % val)
+            f.write("\n")
+        f.close()
+
+        f = open('hidden_to_category.txt', 'w')
+        for syn in self.hidden_to_category:
+            for val in syn:
+                f.write("%s " % val)
+            f.write("\n")
+        f.close()
+
     """
     single rows are used to represent 2-dimensional data sets for ease of computation.
 
@@ -50,7 +96,7 @@ class model:
     with the first four being size and the second being angle. The second set of 8 elements are the negative rule following the same conventions.
     """
 
-    #generate random activations for the three stimuli layers
+    #generate random activations for the three stimuli layers. must be called before every call to genRules/Target
     def genStim(self): 
         self.stim_size = np.random.randint(4)
         self.stim_angle = np.random.randint(4)
@@ -75,74 +121,81 @@ class model:
             d_stim += [.091, .202, .449, 1.0]
 
         d_stim.append(self.sparsity_bias)
-        self.degraded_stim = np.array(d_stim).reshape(1, -1)
+        
 
         inst = []
         for i in range(4):
             for j in range(4, 8):
-              inst.append(self.sigmoid(self.degraded_stim[0][i] * self.degraded_stim[0][j]))
+              inst.append(self.sigmoid(d_stim[i] * d_stim[j]))
         inst.append(self.sparsity_bias)
-        self.instances = np.array(inst).reshape(1, -1)
+        #returns activations for both degraded stimulus and instances layers
+        return (np.array(d_stim), np.array(inst))
 
     #generate random rule combinations
-    def genRules(self, complex_rule): #generate random rule combinations
+    #when color is 0, rules are created such that the target will be black. 1 is for white, and with no arg it's random
+    #only call this after generating stimuli
+    def genRules(self, complex_rule, color=2): 
         r = [0]*17
-        for j in range(8):#positive rule inputs
+        if(color == 0): #ensure that matching rules are chosen to satisfy black condition
+            r[self.stim_size] = 1
+            r[self.stim_angle+4] = 1
+        #random rule selection
+        for j in range(8):              
             x = np.random.random()
             if(x >= self.pos_rule_dist):
                 r[j] = 1
-        if(complex_rule):#negative rule inputs stay zero if simple rule is called for
+        if(complex_rule):
             for k in range(8, 16):
                 x = np.random.random()
                 if(x >= self.neg_rule_dist):
                     r[k] = 1
-        r[16] = 1 #bias unit
-        self.rules = np.array(r).reshape(1, -1)
+            if(color == 0): #nullify conflicting negative rules if black is requested
+                r[self.stim_size+8] = 0
+                r[self.stim_angle+12] = 0
 
-    #determines the target output from given rules & stimuli
+        if(color == 1): #ensure that matching rules aren't chosen to satisfy white condition
+            r[self.stim_size] = 0
+            r[self.stim_angle+4] = 0
+        r[16] = 1 #bias unit
+        return np.array(r)
+
+    #determines the target output from given rules & stimuli when rules are random
     def genTarget(self, complex_rule):
         #check for discrepancies between stimulus and positive rule, if any are found then the target is white (otherwise it's black)
         if(self.rules[0][self.stim_size] != 1):
-            return [0, 1]
+            return np.array([0, 1])
         if(self.rules[0][self.stim_angle + 4] != 1):
-            return [0, 1]
+            return np.array([0, 1])
         #check for discrepancies between stimulus and negative rule if there is one
         if(complex_rule):
             if(self.rules[0][self.stim_size + 8] == 1):
-                return [0, 1]
+                return np.array([0, 1])
             if(self.rules[0][self.stim_size + 12] == 1):
-                return [0, 1]
-        return [1, 0]
-        
-    #populates the input set consisting of rules, stimuli, and the correct output for 1 trial
-    def genInput(self, complex_rule=True): 
-        self.genStim()
-        self.genRules(complex_rule)
-        self.target = np.array(self.genTarget(complex_rule)).reshape(1, -1)
-        
+                return np.array([0, 1])
+        return np.array([1, 0])
+
+    #populates the input activations a randomly selected white/black training example
+    def genInput(self, complex_rule=True):
+        stim = self.genStim()
+        self.degraded_stim = stim[0].reshape(1, -1)
+        self.instances = stim[1].reshape(1, -1)
+        self.rules = self.genRules(complex_rule, np.random.randint(2)).reshape(1, -1)
+        self.target = self.genTarget(complex_rule).reshape(1, -1)
+
     #calculate activations for working memory, hidden and category layers
     def feedForward(self, instructed=True):
-
         if(instructed):
             self.working = self.sigmoid(np.dot(self.rules, self.rules_to_working)).reshape(1, -1)
             self.working[0][24] = self.sparsity_bias
         else:
             self.working = np.array(0*[25]).reshape(1, -1)
 
-        self.hidden = self.sigmoid(np.dot(self.working, self.working_to_hidden) + np.dot(self.degraded_stim, self.degraded_to_hidden)).reshape(1, -1)
+        self.hidden = self.sigmoid(np.dot(self.working, self.working_to_hidden) + np.dot(self.degraded_stim, self.degraded_to_hidden))
         self.hidden[0][24] = self.sparsity_bias
         self.category = self.sigmoid(np.dot(self.hidden, self.hidden_to_category) + np.dot(self.instances, self.inst_to_category))
-        self.category = self.softmax(self.category[0]).reshape(1, -1)
-
-        """if(prnt == 1):
-            print("instance activations: " + str(self.instances))
-            print("instance weights: " + str(self.inst_to_category))
-            print("result of dot: " + str(self.sigmoid(np.dot(self.instances, self.inst_to_category))))
-            print(self.category) """
 
     #calculate and apply weight changes for all synapse groups
     def feedBackward(self):
-    
         out_error = self.target - self.category
         out_delta = out_error*self.sigmoid(self.category, True)
 
@@ -172,49 +225,24 @@ class model:
             self.feedForward()
             self.feedBackward()
             if(i % 100000 == 0):
-                print("rules: " + str(self.rules) + ", square was size " + str(self.stim_size) + " and angle " + str(self.stim_angle))
-                print("resulting target: " + str(self.target))
-                print("output: " + str(self.category))
-                print("error: " + str(np.mean(np.square(self.target - self.category))))
+                print("rules: \n" + str(self.rules) + "\nsquare was size " + str(self.stim_size) + " and angle " + str(self.stim_angle))
+                print("targets: \n" + str(self.target))
+                print("output: \n" + str(self.category))
+                print("error: \n" + str(np.mean(np.square(self.target - self.category))))
                 print("_______")
-            
-    #exports each weight matrix to 5 files
-    def export(self): 
-        f = open('degraded_to_hidden.txt', 'w')
-        for syn in self.degraded_to_hidden:
-            f.write("%s\n" % syn)
-        f.close()
-
-        f = open('rules_to_working.txt', 'w')
-        for syn in self.rules_to_working:
-            f.write("%s\n" % syn)
-        f.close()
-
-        f = open('inst_to_category.txt', 'w')
-        for syn in self.inst_to_category:
-            f.write("%s\n" % syn)
-        f.close()
-
-        f = open('working_to_hidden.txt', 'w')
-        for syn in self.working_to_hidden:
-            f.write("%s\n" % syn)
-        f.close()
-
-        f = open('hidden_to_category.txt', 'w')
-        for syn in self.hidden_to_category:
-            f.write("%s\n" % syn)
-        f.close()
-
+    
     #tests the network on 10 random inputs
     def test(self): 
         for i in range(11):
             self.genInput()
             self.feedForward()
-            print("rules: " + str(self.rules) + ", square was size " + str(self.stim_size) + " and angle " + str(self.stim_angle))
+            print("rules: " + str(self.rules) + ", \nsquare was size " + str(self.stim_size) + " and angle " + str(self.stim_angle))
             print("resulting target: " + str(self.target))
             print("output: " + str(self.category))
             print("error: " + str(np.mean(np.square(self.target - self.category))))
             print("_______")
+
+            
     
 
 
